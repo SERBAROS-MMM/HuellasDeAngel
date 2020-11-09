7322aa2674666a5ed48069890ca2cd340dc0315e
Sergio Barrientos O <59656107+SergioBtosO@users.noreply.github.com>
2020-11-09 11:09:54 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+//layouts
+import Admin from "views/layouts/Admin"
+
+// pages for this product
+import LoginPage from "views/pages/LoginPage.js";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/Login" component={LoginPage} />
+      <Redirect from="/" to="/admin/" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/views/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "components/dashboard/CustomButtons/Button.js";
+  
+
+const HomePage = (props) => {
+
+    if(props.active)
+    {
+        window.location.href = '/login'
+    }
+
+    const cerrarSesion = (e) =>{
+        window.location.href = '/login'
+    }
+
+  return (
+    <div>
+      <Button variant="text" color="primary" onClick={cerrarSesion}>
+        CerrarSesion
+      </Button>
+    </div>
+  );
+};
+
+export default HomePage;
