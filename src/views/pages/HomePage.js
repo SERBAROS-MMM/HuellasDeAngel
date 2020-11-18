1d3b6a1c240ebffcca36acdef91e3a41c19c66a3
unknown <morian.monsalve@gmail.com>
2020-11-17 23:15:33 -0500

    GridHome, NotifacionLogin, Indicadores

--- src/views/pages/HomePage.js
+++ src/views/pages/HomePage.js
@@ -1,40 +1,19 @@
 import React,{useState} from "react";
 import Button from "components/dashboard/CustomButtons/Button.js";
 import Badge from "components/kit/Badge/Badge.js"
+import Indicadores from "components/huellas/Indicadores.js"
+import CardBoy from "components/huellas/CardBoy.js"
+import Buscar from "components/huellas/Buscar.js"
 
 const HomePage = (props) => {
-
-    const [numNino, setNumNino] = useState(0);
-
-    const sumarNino = (e) =>{
-      setNumNino(numNino + 1)
-    }
-
-    if(props.active)
-    {
-        window.location.href = '/login'
-    }
-
-    const cerrarSesion = (e) =>{
-        window.location.href = '/login'
-    }
-
   return (
     <div>
       <div>
-      <Button variant="text" color="primary" onClick={cerrarSesion}>
-        CerrarSesion
-      </Button>
-      </div>
-      <div>
-        <Badge color="primary">
-          Niños {numNino}
-          <Button variant="text" color="secundary" onClick={sumarNino}>
-            +
-          </Button>
-        </Badge>        
+        <Indicadores/>
+        <Buscar/>
       </div>
     </div>
+    
       );
 };
 
