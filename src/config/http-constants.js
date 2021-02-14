1c12f5bdb64ad4bca03362d417e3edf251b87487
Sergio Barrientos O <59656107+SergioBtosO@users.noreply.github.com>
2021-02-14 17:08:46 -0500

    ajuste service

--- src/config/http-constants.js
+++ src/config/http-constants.js
@@ -1,9 +1,9 @@
 export const HTTP_CONSTANTS={
-    url:'http://localhost:4000/API/',
-    urlUp:'http://localhost:4000/API/up',
+    //url:'http://localhost:4000/API/',
+    //urlUp:'http://localhost:4000/API/up',
     
-    //url:'http://69.164.206.46:4000/API/',
-    //urlUp:'http://69.164.206.46:4000/API/up',
+    url:'http://69.164.206.46:4000/API/',
+    urlUp:'http://69.164.206.46:4000/API/up',
     login:'auth/login',
     autologin:'auth/autologin',
     persons:'persons/',
